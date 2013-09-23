69678141d4b1fedfc7846ce368fd0d848e39dee9
viperjuga <viperjuga13@inbox.lv>
2013-09-23 22:28:46 +0300

    Startpoint of application

--- src/com/MyTwitter.java
+++ src/com/MyTwitter.java
@@ -8,12 +8,37 @@ import com.vaadin.ui.*;
  * Time: 9:45 PM
  */
 public class MyTwitter extends Application {
+    Window mWindow;
+    TextField mUserName;
+    PasswordField mPassword;
+    Button mLogin;
 
+    //<editor-fold desc="Constructor">
+    public MyTwitter(){
+        mWindow = new Window("Twitter Application");
+        mUserName = new TextField("Username");
+        mPassword = new PasswordField("Password");
+        mLogin = new Button("Login",btn_login_click);
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="General method">
     @Override
     public void init() {
-        Window mainWindow = new Window("Twitter Application");
-        Label label = new Label("Hello, it is start point of Twitter application");
-        mainWindow.addComponent(label);
-        setMainWindow(mainWindow);
+        mWindow.addComponent(mUserName);
+        mWindow.addComponent(mPassword);
+        mWindow.addComponent(mLogin);
+        setMainWindow(mWindow);
     }
+    //</editor-fold>
+
+    //<editor-fold desc="Listeners">
+    Button.ClickListener btn_login_click = new Button.ClickListener(){
+        @Override
+        public void buttonClick(Button.ClickEvent clickEvent) {
+            String yourAcc = String.format("Your name:%s Your password:%s",mUserName,mPassword);
+            mWindow.showNotification(yourAcc);
+        }
+    };
+    //</editor-fold>
 }
